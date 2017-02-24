94fe1fea1ba6841aa3eb8c0b3847f7bf8c561593
Alex Aheyev <ah.alesha@gmail.com>
2017-02-25 01:40:53 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package pkg5;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> string = new ArrayList<>();
+        string.add("1");
+        string.add("2");
+        string.add("3");
+        int a = 2;
+        System.out.println(string.get(a)+a);
+    }
+    
+}
