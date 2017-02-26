93b777a61ec2017e30c563ce22c9c08cf1d62d95
Alex Aheyev <ah.alesha@gmail.com>
2017-02-26 02:57:17 +0200

    Update Main.java
    correct adding of index in array

--- Main.java
+++ Main.java
@@ -1,16 +1,20 @@
-package pkg5;
+package com.company;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
         ArrayList<String> string = new ArrayList<>();
-        string.add("1");
-        string.add("2");
-        string.add("3");
-        int a = 2;
-        System.out.println(string.get(a)+a);
+        string.add("a");
+        string.add("b");
+        string.add("c");
+        for (int i = 0; i < string.size(); i++) {
+            Integer x = new Integer(i+1);
+            System.out.println(string.get(i));
+            string.set(i,string.get(i)+x.toString());
+            System.out.println(string.get(i));
+        }
     }
-    
 }
